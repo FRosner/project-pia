15f24f08923ad39a6bff1f6f51224eb8f1fabbd6
Frank Rosner <frank@fam-rosner.de>
2015-09-29 18:25:08 +0200

    reduce number of concurrent HTTP handlers to 1

--- src/main/scala/de/frosner/pia/Options.scala
+++ src/main/scala/de/frosner/pia/Options.scala
@@ -41,7 +41,7 @@ object Options {
     }
   }
 
-  val concurrentHttpConnections = 2
+  val concurrentHttpConnections = 1
 
   private val DEFAULT_R_SERVER_INTERFACE = "127.0.0.1"
   val rServerInterface = Option(System.getProperty("pia.rServerInterface")).getOrElse {
