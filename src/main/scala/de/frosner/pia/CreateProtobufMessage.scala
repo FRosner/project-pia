f343650e665c6eb7d189c99da56c9ebf6dc0aaae
Frank Rosner <frank@fam-rosner.de>
2015-09-29 10:30:48 +0200

    some protobuf test code

--- src/main/scala/de/frosner/pia/CreateProtobufMessage.scala
+++ src/main/scala/de/frosner/pia/CreateProtobufMessage.scala
@@ -8,7 +8,14 @@ object CreateProtobufMessage extends App {
 
   for (i <- 1 to 10) {
     val out = new FileOutputStream(s"$i.obs")
-    Observation.newBuilder().setDoubleFeature(i).build().writeTo(out)
+    val obsI = Observation.newBuilder().setDoubleFeature(i).build()
+    obsI.writeTo(out)
+    val observationsProto = Observations.getDescriptor
+    println(observationsProto.getName)
+    val observation = observationsProto.getMessageTypes.get(0)
+    println(observation.getName)
+    val firstFeature = observation.getFields.get(0)
+    println(firstFeature.getName)
     out.close()
   }
 
